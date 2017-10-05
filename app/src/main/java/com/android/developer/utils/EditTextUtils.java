004526aa023b5931e5344dded0763361a637d514
yibas <naruto@micous.com>
2017-10-05 21:33:23 +0800

    Initial commit

---
+++ app/src/main/java/com/android/developer/utils/EditTextUtils.java
@@ -0,0 +1,58 @@
+package com.android.developer.utils;
+
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Naruto on 2017/10/5.
+ */
+
+public class EditTextUtils {
+
+    /**
+     * 过滤掉常见特殊字符,常见的表情
+     */
+    public static void setEtFilter(EditText editText) {
+        if (editText == null) {
+            return;
+        }
+        //表情过滤器
+        InputFilter emojiFilter = new InputFilter() {
+
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest,
+                                       int dstart, int dend) {
+                Pattern emoji = Pattern.compile(
+                        "[\ud83c\udc00-\ud83c\udfff]|[\ud83d\udc00-\ud83d\udfff]|[\u2600-\u27ff]",
+                        Pattern.UNICODE_CASE | Pattern.CASE_INSENSITIVE);
+                Matcher emojiMatcher = emoji.matcher(source);
+                if (emojiMatcher.find()) {
+                    return "";
+                }
+                return null;
+            }
+        };
+        //特殊字符过滤器
+        InputFilter specialCharFilter = new InputFilter() {
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                String regexStr = "[`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+                Pattern pattern = Pattern.compile(regexStr);
+                Matcher matcher = pattern.matcher(source.toString());
+                if (matcher.matches()) {
+                    return "";
+                } else {
+                    return null;
+                }
+
+            }
+        };
+
+        editText.setFilters(new InputFilter[]{emojiFilter, specialCharFilter,new InputFilter.LengthFilter(20)});
+    }
+
+}
